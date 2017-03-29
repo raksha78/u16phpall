5b0fc70568d45b4a8c5defb78638a6cecfec2b5b
Ragesh Krishna <ragesh@rageshkrishna.com>
2017-03-29 15:16:12 +0530

    Add mongodb for PHP 7.x

--- version/7_0.sh
+++ version/7_0.sh
@@ -32,4 +32,8 @@ printf '\n' | bin/pickle install amqp-1.6.0
 printf '\n' | bin/pickle install zmq-beta
 printf '\n' | bin/pickle install redis
 
+echo "--with-openssl-dir=yes" >> /tmp/pickle-mongodb-opts
+printf '\n' | bin/pickle install --with-configure-options=/tmp/pickle-mongodb-opts mongodb
+rm /tmp/pickle-mongodb-opts
+
 cd /
